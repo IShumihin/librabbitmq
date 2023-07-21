8d69e0204678cc05a95c9541cf5790b201c37c10
IShumihin <shumidze@gmail.com>
2023-07-21 13:32:58 +0400

    Bumped version to 2.0.2

--- librabbitmq/__init__.py
+++ librabbitmq/__init__.py
@@ -19,7 +19,7 @@ ConnectionError = _librabbitmq.ConnectionError
 ChannelError = _librabbitmq.ChannelError
 
 
-__version__ = '2.0.1'
+__version__ = '2.0.2'
 __all__ = ['Connection', 'Message', 'ConnectionError', 'ChannelError']
 
 
